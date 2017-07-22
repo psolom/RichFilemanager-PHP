c2c5b42ecc51eaa6d953a185121491d5a4e18c55
pavel <solomienko1802@gmail.com>
2017-07-22 11:22:35 +0300

    bugfix: logger error upon define user IP

--- src/Logger.php
+++ src/Logger.php
@@ -83,8 +83,8 @@ class Logger {
      */
     protected function getUserIp()
     {
-        $client  = @$_SERVER['HTTP_CLIENT_IP'];
-        $forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
+        $client  = isset($_SERVER['HTTP_CLIENT_IP']) ? $_SERVER['HTTP_CLIENT_IP'] : null;
+        $forward = isset($_SERVER['HTTP_X_FORWARDED_FOR']) ? $_SERVER['HTTP_X_FORWARDED_FOR'] : null;
         $remote  = $_SERVER['REMOTE_ADDR'];
 
         if (filter_var($client, FILTER_VALIDATE_IP)) {
