0d646f4fe450a893b97d3da35d2d048c3bb3f7f3
pavel <solomienko1802@gmail.com>
2017-10-22 16:44:05 +0300

    added ItemInterface interface

---
+++ src/Repository/ItemInterface.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace RFM\Repository;
+
+interface ItemInterface
+{
+    /**
+     * Get storage instance associated with model item.
+     *
+     * @return StorageInterface
+     */
+    public function getStorage();
+
+    /**
+     * Associate storage with model item.
+     *
+     * @param StorageInterface $storageName
+     */
+    public function setStorage($storageName);
+
+    /**
+     * Return thumbnail relative path from given path.
+     * Work for both files and dirs paths.
+     *
+     * @return string
+     */
+    public function getThumbnailPath();
+}
