4f005d98cfee1e2bb45d15e323be104f37f28951
pavel <solomienko1802@gmail.com>
2017-11-02 15:39:18 +0200

    bump version to 1.1.2

--- src/Application.php
+++ src/Application.php
@@ -428,6 +428,6 @@ class Application extends Container {
      */
     public function version()
     {
-        return 'RichFilemanager PHP connector v1.1.1';
+        return 'RichFilemanager PHP connector v1.1.2';
 }
 }
